d59c7a714814c93a0d2676ce5a52228356f78fcd
Gorgostammos <90147575+Gorgostammos@users.noreply.github.com>
2025-12-07 00:40:11 +0100

    byttet fra icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faJava } from '@fortawesome/free-brands-svg-icons'
-import { faPython } from '@fortawesome/free-brands-svg-icons'
-import { faHtml5 } from '@fortawesome/free-brands-svg-icons'
-import { faCss } from '@fortawesome/free-brands-svg-icons'
-import { faReact } from '@fortawesome/free-brands-svg-icons'
-
-
-
 import './App.css'
 
 function App() {
@@ -18,29 +9,38 @@ function App() {
   return (
     <>
       <div>
-       <h1>Protofolio</h1>
-       <div className='omMeg'>
-       <h2 id='omMeg'>Om meg</h2>
-       <p id='omMeg'>Jeg er en mann på 22 år og har fullført bachelorgrad innen informasjonssystemer,
-        med spesialisering i Software Engineering og Business Intelligence. 
-        Jeg trivesgodt både i selvstendig prosjekter og i teammiljøer, og jeg er spesielt glad i programmering.</p>
-       </div>
-       <div className='Ferdighetter'>
-       <h2>Ferdighetter</h2>
-       <div id='Mine'>
-       <FontAwesomeIcon icon={faJava} />
-       <FontAwesomeIcon icon={faPython} />
-       <FontAwesomeIcon icon={faHtml5} />
-       <FontAwesomeIcon icon={faCss} />
-       <FontAwesomeIcon icon={faReact} />
-       </div>
-       </div>
+        <h1>Protofolio</h1>
 
-       <h2>Programering språk</h2>
+        <div className='omMeg'>
+          <h2 id='omMeg'>Om meg</h2>
+          <p id='omMeg'>
+            Jeg er en mann på 22 år og har fullført bachelorgrad innen informasjonssystemer,
+            med spesialisering i Software Engineering og Business Intelligence. 
+            Jeg trives godt både i selvstendige prosjekter og i teammiljøer, 
+            og jeg er spesielt glad i programmering.
+          </p>
+        </div>
+
+        <div className='Ferdigheter'>
+          <h2>Ferdigheter</h2>
+          <div id='Mine' className="icons">
+            
+          <i class="devicon-java-plain-wordmark colored"></i>
+          
+            <i class="devicon-python-plain-wordmark colored"></i>
+          
+            <i class="devicon-html5-plain-wordmark colored"></i>
+            <i className="devicon-css3-plain-wordmark colored"></i>
+            
+            <i class="devicon-react-original-wordmark colored"></i>
+          
+            <i className="devicon-kotlin-plain-wordmark"></i>   {/* Kotlin ikonet */}
+          </div>
+        </div>
+
+        <h2>Programmeringsspråk</h2>
       </div>
-      
     </>
-    
   )
 }
 
